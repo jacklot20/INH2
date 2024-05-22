b9caa22fa974d37d397589ffa32b7633df7eb88f
jacklot20 <143613210+jacklot20@users.noreply.github.com>
2024-05-21 22:53:09 -0400

    Delete Unit7/script.js

--- Unit7/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("myButton").addEventListener("click", function() {
-    document.getElementById("greeting").innerText = "Hello, JavaScript is working!";
-    this.innerText = "Clicked!";
-});
